f5b38432b9974a8e6b52083591f04b56cd900106
Miguel Sampaio <sampaio.veiga@gmail.com>
2013-10-11 12:58:28 +0100

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transportes::Application.config.secret_key_base = '3cac00537f1a95b07413f324df2707eccac95eaa01646dff793f74c4bdc0a71ea18eaaf83a2bc1286ddcdc9a0553401ab8ea805ed27f450e2e1f536760a9821e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transportes::Application.config.session_store :cookie_store, key: '_transportes_session'
