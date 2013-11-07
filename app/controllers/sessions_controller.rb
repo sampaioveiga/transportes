78f5df9e1e00dec0f7f36c1a4e3a10038ca86f91
Miguel Sampaio <sampaio.veiga@gmail.com>
2013-11-07 11:29:31 +0000

    Added not auth 2 new user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_numero_mecanografico(params[:numero_mecanografico])
-		if user && user.authenticate(params[:password])
+		if user && user.authenticate(params[:password]) && user.auth
 			session[:user_id] = user.id
 			flash[:success] = "Sessão iniciada, #{user.nome}"
 			redirect_to root_path
